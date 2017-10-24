278826468ab85497cbe472c6716230f82a4d92e9
Germ90 <germanrios90@gmail.com>
2017-10-24 16:19:10 -0300

    Update IArbolTrie.java

--- AlasUcu/src/alasucu/Trie/IArbolTrie.java
+++ AlasUcu/src/alasucu/Trie/IArbolTrie.java
@@ -24,7 +24,26 @@ public interface IArbolTrie {
     */
     void insertar(String unId,String palabra);
 
-    
+    /**
+    * Listado de palabras que comienzan con el parametro recibido.
+    *
+    * @param prefijo prefijo a buscar
+    * @return Lsita con las palabras que comienzan con prefijo
+    */
     LinkedList<String> predecir(String prefijo);
     
+    /**
+    * Identificador de la palabra
+    *
+    * @param prefijo prefijo a buscar
+    * @return Etiqueta de la palabra
+    */
+    public Comparable buscarEtiqueta(String prefijo);
+    
+     /**
+    * Listado de etiquetas de las palabras del trie.
+    *
+    * @return Lsita con las etiquetas de las palabras del trie
+    */
+    public LinkedList<Comparable> getEtiquetas()
 }
