d635e75ba9c328e2c77119ebaf4e01e5e0c9a4af
Germ90 <germanrios90@gmail.com>
2017-10-24 16:34:07 -0300

    Update INodoTrie.java

--- AlasUcu/src/alasucu/Trie/INodoTrie.java
+++ AlasUcu/src/alasucu/Trie/INodoTrie.java
@@ -9,24 +9,76 @@ import java.util.LinkedList;
  */
 public interface INodoTrie {
 
+    /**
+    * Operacion para obtener los hijos del nodo.
+    * 
+    * @return coleccion de hijos del nodo
+    */
     public Map<Comparable, TNodoTrie> getHijos();
     
+    /**
+    * Saber si el nodo es palabra
+    *
+    * @return true en caso de ser palabra, false en caso contrario.
+    */
     public boolean isEsPalabra();
     
+    /**
+    * Setear booleano esPalabra
+    *
+    * @param valor a setear
+    */
     public void setEsPalabra(boolean esPalabra);
     
+    /**
+    * Obtener etiqueta del nodo
+    *
+    * @return Etiqueta del nodo
+    */
     public Comparable getEtiqueta();
         
+    /**
+    * Setear etiqueta del nodo
+    *
+    * @param Etiqueta a setear
+    */
     public void setEtiqueta(Comparable etiqueta);
     
+    /**
+    * Imprime las palabras del trie
+    *
+    */
     void imprimir();
 
+    /**
+    * Listado de etiquetas de las palabras del trie.
+    *
+    * @return Lsita con las etiquetas de las palabras del trie
+    */
     public LinkedList<Comparable> getEtiquetas();
         
+    /**
+    * Obtener nodo con la ultima letra del prefijo pasado como parametro
+    *
+    * @param parcial del prefijo
+    * @return Nodo con la ultima letra de la cadena s
+    */
     public TNodoTrie buscarNodoTrie(String s);
     
+    /**
+    * Insertar una nueva palabra en el trie
+    *
+    * @param unId     Id de la palabra
+    * @param palabra  Palabra a insertar en el trie
+    */
     void insertar(String unId,String unaPalabra);
     
+    /**
+    * Listado de palabras que comienzan con el parametro recibido.
+    *
+    * @param prefijo prefijo a buscar
+    * @return Lsita con las palabras que comienzan con prefijo
+    */
     public void predecir(String prefijo, LinkedList<String> palabras);
     
 }
