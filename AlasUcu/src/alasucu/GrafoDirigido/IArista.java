2291acdee2f30b5ec0bf62ca835d2b50bb978004
Germ90 <germanrios90@gmail.com>
2017-10-24 15:56:17 -0300

    Update IArista.java

--- AlasUcu/src/alasucu/GrafoDirigido/IArista.java
+++ AlasUcu/src/alasucu/GrafoDirigido/IArista.java
@@ -1,27 +1,49 @@
 package alasucu.GrafoDirigido;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author Ernesto
+ * @author German
  */
 public interface IArista {
 
+    /**
+    * Operacion para obtener el costo de la Arista.
+    *
+    * @return Costo de la arista
+    */
     double getCosto();
 
+    /**
+    * Operacion para obtener la etiqueta destino de la Arista.
+    *
+    * @return Etiqueta vertice destino de la arista
+    */
     Comparable getEtiquetaDestino();
 
+    /**
+    * Operacion para obtener la Origen de la Arista.
+    *
+    * @return Identificador del vertice Origen de la arista
+    */
     Comparable getEtiquetaOrigen();
 
+    /**
+    * Operacion para setear costo de la Arista.
+    *
+    */
     void setCosto(double costo);
 
+    /**
+    * Operacion para setear Etiqueta destino de la Arista.
+    *
+    */
     void setEtiquetaDestino(Comparable etiquetaDestino);
-
+    
+    /**
+    * Operacion para setear etiqueta origen de la Arista.
+    *
+    */
     void setEtiquetaOrigen(Comparable etiquetaOrigen);
     
 }
