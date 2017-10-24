0d71f20a00566073551a0c8ccb0ee340d92a3415
Germ90 <germanrios90@gmail.com>
2017-10-24 17:12:06 -0300

    Update TOpcionVuelo.java

--- AlasUcu/src/alasucu/TOpcionVuelo.java
+++ AlasUcu/src/alasucu/TOpcionVuelo.java
@@ -5,12 +5,6 @@ import alasucu.GrafoDirigido.TAdyacencia;
 import alasucu.GrafoDirigido.TVertice;
 import java.util.LinkedList;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author German
